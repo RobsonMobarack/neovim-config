f9a5fbed0fc8a5ff17e2975b13b18cd8be88e909
Robson Mobarack <robson.mobarack@hotmail.com>
2025-10-15 11:13:30 -0300

    feat: added keymaps to work with tmux

--- init.lua
+++ init.lua
@@ -46,6 +46,12 @@ vim.keymap.set("n", "<leader>s", ":write<CR>", { silent = true })
 -- space + h to clear search highlight
 vim.keymap.set("n", "<leader>h", ":noh<CR>", { silent = true })
 
+-- Navigate between nvim and tmux
+-- vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
+-- vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
+-- vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
+-- vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
+
 ---------------------------------------------------------------
 -- =============== Lazy.nvim Bootstrap ========================
 ---------------------------------------------------------------
