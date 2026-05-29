e1eeada2238693ff7750819e937bbe6640fa9a22
Robson Mobarack <robson.mobarack@hotmail.com>
2026-05-29 13:32:08 -0300

    chore(treesitter): pin nvim-treesitter for Neovim 0.11 compatibility
    - Pin nvim-treesitter version to maintain stability, as upstream now targets Neovim 0.12+

--- init.lua
+++ init.lua
@@ -1140,6 +1140,7 @@ require("lazy").setup({
 		-----------------------------------------------------------
 		{
 			"nvim-treesitter/nvim-treesitter",
+			branch = "master",
 			build = ":TSUpdate",
 			config = function()
 				require("nvim-treesitter.configs").setup({
